c79f3b50d62ca584aca1ba2407ff5faadda4314a
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-01-15 20:18:17 +0000

    exersice completed

--- src/js/index.js
+++ src/js/index.js
@@ -12,36 +12,66 @@ import Home from "./component/home.jsx";
 ReactDOM.render(<Home />, document.querySelector("#app"));
 
 //Timer
-let helperCounter = 0;
-let counter = 0;
-let counter2 = 1;
-let counter3 = 1;
+let counter = 999998;
 
 const simpleCounter = () => {
 	const num1 = document.querySelector("#num1");
 	const num2 = document.querySelector("#num2");
 	const num3 = document.querySelector("#num3");
-
-	num1.innerHTML = counter;
-	counter++;
-	helperCounter++;
-	if (counter === 10) {
+	const num4 = document.querySelector("#num4");
+	const num5 = document.querySelector("#num5");
+	const num6 = document.querySelector("#num6");
+	if (String(counter).length === 1) {
+		num1.innerHTML = counter;
+		counter++;
+	} else if (String(counter).length === 2) {
+		let splitStr = String(counter).split("");
+		console.log(splitStr);
+		num1.innerHTML = splitStr[1];
+		num2.innerHTML = splitStr[0];
+		counter++;
+	} else if (String(counter).length === 3) {
+		let splitStr = String(counter).split("");
+		console.log(splitStr);
+		num1.innerHTML = splitStr[2];
+		num2.innerHTML = splitStr[1];
+		num3.innerHTML = splitStr[0];
+		counter++;
+	} else if (String(counter).length === 4) {
+		let splitStr = String(counter).split("");
+		console.log(splitStr);
+		num1.innerHTML = splitStr[3];
+		num2.innerHTML = splitStr[2];
+		num3.innerHTML = splitStr[1];
+		num4.innerHTML = splitStr[0];
+		counter++;
+	} else if (String(counter).length === 5) {
+		let splitStr = String(counter).split("");
+		console.log(splitStr);
+		num1.innerHTML = splitStr[4];
+		num2.innerHTML = splitStr[3];
+		num3.innerHTML = splitStr[2];
+		num4.innerHTML = splitStr[1];
+		num5.innerHTML = splitStr[0];
+		counter++;
+	} else {
+		let splitStr = String(counter).split("");
+		console.log(splitStr);
+		num1.innerHTML = splitStr[5];
+		num2.innerHTML = splitStr[4];
+		num3.innerHTML = splitStr[3];
+		num4.innerHTML = splitStr[2];
+		num5.innerHTML = splitStr[1];
+		num6.innerHTML = splitStr[0];
+		counter++;
+	}
+	if (counter === 1000001) {
 		counter = 0;
-	}
-	if (counter2 === 10) {
-		counter2 = 0;
-	}
-	if (counter3 === 10) {
-		counter3 = 0;
-	}
-	if (helperCounter % 11 === 0) {
-		num2.innerHTML = counter2;
-		counter2++;
-		helperCounter++;
-	}
-	if (helperCounter % 110 === 0) {
-		num3.innerHTML = counter3;
-		counter3++;
+		num2.innerHTML = 0;
+		num3.innerHTML = 0;
+		num4.innerHTML = 0;
+		num5.innerHTML = 0;
+		num6.innerHTML = 0;
 	}
 };
 
